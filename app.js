8a84aa00ecc61823c01bdb766b56df330e9cd6d7
bharding2 <b.harding@gmail.com>
2017-06-08 18:54:11 -0700

    so those promises work

--- app.js
+++ app.js
@@ -10,11 +10,30 @@ var params = {
   lang: 'en',
 };
 
-function getTweets(params) {
+function getSlothbearTweets(params) {
   return new Promise((resolve, reject) => {
     slothbearBot.get('search/tweets', params, (err, tweets, res) => {
       if (err) return reject(err);
       resolve(tweets);
+    });
+  });
+}
+
+function favoriteSlothbearTweets(tweets) {
+  return new Promise((resolve, reject) => {
+    tweets.statuses.forEach((tweet) => {
+      let statusId = {
+        id: tweet.id_str
+      };
+
+      slothbearBot.post('favorites/create', statusId, (err, res) => {
+        if (err) return reject(err);
+
+        let username = res.user.screen_name;
+        let tweetId = res.id_str;
+
+        console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`);
+      });
     });
   });
 }
