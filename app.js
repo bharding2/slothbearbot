911ed7a029c168d02567740f54fe4bee6d5bd991
bharding2 <b.harding@gmail.com>
2017-06-25 20:10:51 -0700

    modularization

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const Twitter = require('twitter');
-const slothbearBot = new Twitter(require('./config.js'));
+// const Twitter = require('twitter');
+// const slothbearBot = new Twitter(require('./config.js'));
 
 // hmmm...  how to module.export this?  a function that takes a params argument and then runs the following, or a set of functions as tools.
+
+const slothBot = require('./lib/slothbot.js');
 
 var params = {
   q: process.argv[2] || '#slothbears',
@@ -10,34 +12,36 @@ var params = {
   lang: 'en',
 };
 
-function getSlothbearTweets(params) {
-  return new Promise((resolve, reject) => {
-    slothbearBot.get('search/tweets', params, (err, tweets) => {
-      if (err) return reject(err);
-      resolve(tweets);
-    });
-  });
-}
+slothBot(params);
 
-function favoriteSlothbearTweets(tweets) {
-  return new Promise((resolve, reject) => {
-    tweets.statuses.forEach((tweet) => {
-      let statusId = {
-        id: tweet.id_str
-      };
-
-      slothbearBot.post('favorites/create', statusId, (err, res) => {
-        if (err) return reject(err);
-
-        let username = res.user.screen_name;
-        let tweetId = res.id_str;
-
-        console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`);
-      });
-    });
-  });
-}
-
-getSlothbearTweets(params)
-  .then(favoriteSlothbearTweets)
-  .catch(console.log);
+// function getSlothbearTweets(params) {
+//   return new Promise((resolve, reject) => {
+//     slothbearBot.get('search/tweets', params, (err, tweets) => {
+//       if (err) return reject(err);
+//       resolve(tweets);
+//     });
+//   });
+// }
+//
+// function favoriteSlothbearTweets(tweets) {
+//   return new Promise((resolve, reject) => {
+//     tweets.statuses.forEach((tweet) => {
+//       let statusId = {
+//         id: tweet.id_str
+//       };
+//
+//       slothbearBot.post('favorites/create', statusId, (err, res) => {
+//         if (err) return reject(err);
+//
+//         let username = res.user.screen_name;
+//         let tweetId = res.id_str;
+//
+//         console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`);
+//       });
+//     });
+//   });
+// }
+//
+// getSlothbearTweets(params)
+//   .then(favoriteSlothbearTweets)
+//   .catch(console.log);
