cc349a1908ad4037038e2cff9a62c45b76dd19aa
bharding2 <b.harding@gmail.com>
2017-06-26 18:30:56 -0700

    cleanup

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// const Twitter = require('twitter');
-// const slothbearBot = new Twitter(require('./config.js'));
-
-// hmmm...  how to module.export this?  a function that takes a params argument and then runs the following, or a set of functions as tools.
-
 const slothBot = require('./lib/slothbot.js');
 
 var params = {
@@ -13,35 +8,3 @@ var params = {
 };
 
 slothBot(params);
-
-// function getSlothbearTweets(params) {
-//   return new Promise((resolve, reject) => {
-//     slothbearBot.get('search/tweets', params, (err, tweets) => {
-//       if (err) return reject(err);
-//       resolve(tweets);
-//     });
-//   });
-// }
-//
-// function favoriteSlothbearTweets(tweets) {
-//   return new Promise((resolve, reject) => {
-//     tweets.statuses.forEach((tweet) => {
-//       let statusId = {
-//         id: tweet.id_str
-//       };
-//
-//       slothbearBot.post('favorites/create', statusId, (err, res) => {
-//         if (err) return reject(err);
-//
-//         let username = res.user.screen_name;
-//         let tweetId = res.id_str;
-//
-//         console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`);
-//       });
-//     });
-//   });
-// }
-//
-// getSlothbearTweets(params)
-//   .then(favoriteSlothbearTweets)
-//   .catch(console.log);
